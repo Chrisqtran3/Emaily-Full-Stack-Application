6ce9d5a6c53794bd8a4204ed0379cf3a359006c5
Chris <tran.chris10@gmail.com>
2018-01-31 16:54:30 -0600

    Add check for user creation

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys')
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy({
@@ -8,8 +11,13 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    console.log('Access Token:', accessToken);
-    console.log('Refresh Token:', refreshToken);
-    console.log('Profile', profile);
+    User.findOne({ googleID: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+
+        } else {
+          new User({ googleID: profile.id }).save();
+        }
+      })
   })
 );
