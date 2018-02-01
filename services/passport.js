17abce718eb708e741f9004a2d69ac36a63b26e4
Chris <tran.chris10@gmail.com>
2018-02-01 12:20:17 -0600

    Finish authentication

--- services/passport.js
+++ services/passport.js
@@ -5,19 +5,30 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleID: profile.id })
-      .then((existingUser) => {
-        if (existingUser) {
-
-        } else {
-          new User({ googleID: profile.id }).save();
-        }
+    User.findOne({ googleID: profile.id }).then((existingUser) => {
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleID: profile.id }).save().then(user => {
+          done(null, user);
+        })
+      }
       })
   })
 );
