29b61c7c5138edc808fb79424640d65f394a6fcb
Chris <tran.chris10@gmail.com>
2018-02-02 10:06:54 -0600

    Set up redux and react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header'
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>Survey New</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
-    <div>Hi There!</div>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path='/' component={Landing} exact />
+          <Route path='/surveys' component={Dashboard} exact />
+          <Route path='/surveys/new' component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
