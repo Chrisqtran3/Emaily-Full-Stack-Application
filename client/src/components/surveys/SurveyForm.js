79aaa86ee0da0fac6ef791ac038eeb002cb7c6db
Chris <tran.chris10@gmail.com>
2018-02-21 10:32:23 -0600

    Validate emails

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   {
@@ -47,6 +48,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,3 +1,15 @@
-export default (emails) => {
-  const emailsArray = emails.split(',').map(email => email.trim());
+const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export default emails => {
+  const invalidEmails = emails
+    .split(',')
+    .map(email => email.trim())
+    .filter(email => re.test(email) === false);
+
+  if (invalidEmails.length) {
+    console.log('Invalid: ', invalidEmails);
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+
+  return;
 }
